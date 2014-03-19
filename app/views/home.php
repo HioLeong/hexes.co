c3888fb5313e81c6f0a1d2bc431387fa31201534
hioleong <hiowengleong@gmail.com>
2014-03-19 18:04:24 +0000

    Integrated new view

--- app/views/home.php
+++ app/views/home.php
@@ -1,24 +1,16 @@
-<!DOCTYPE HTML style="height: 100% !important;">
-
-<!--
-	Striped 2.5 by HTML5 Up!
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-
+<!DOCTYPE html style="height: 100% !important;">
 <html>
-	<head>
-		<title>Hexagon</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700|Open+Sans+Condensed:300,700" rel="stylesheet" />
-		<script src="js/jquery.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-panels.min.js"></script>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Hexes</title>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/main.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/jquery.min.js"></script>
 		<script src="js/init.js"></script>
-        <script src="d3.v3.min.js"></script>
-		<script src="d3.hexbin.v0.min.js"></script>
 		<script src="js/angular.js"></script>
 		<script src="js/angular-route.js"></script>
 
@@ -41,151 +33,51 @@
         <link rel="stylesheet" href="css/loginBox.css"/>
         <link rel="stylesheet" href="css/style.css"/>
         <link rel="stylesheet" href="css/personalNewsFeed.css"/>
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
-		<!--[if lte IE 7]><link rel="stylesheet" href="css/ie7.css" /><![endif]-->
-	</head>
-	<!--
-		Note: Set the body element's class to "left-sidebar" to position the sidebar on the left.
-		Set it to "right-sidebar" to, you guessed it, position it on the right.
-	-->
-<body class="left-sidebar">
-    <div id="wrapper">
-        <div id="content" style="" ng-app="hexApp">
+<link rel="stylesheet" href="css/mainStyle.css" />
+</head>
 
-            <div ng-view></div>
-            <div id="switcherWrapper">
-        </div>
-        </div>
-    </div>
+<body>
 
-        <div id="sidebar">
-            <div id="logo">
-                <h1>Hexes</h1>
-            </div>
-
-            <div class="nav-wrap">
-                <nav class="main-nav" role="navigation">
-                    <ul class="unstyled list-hover-slide">
-                        <li><a href="#/profile">Profile</a></li>
-                        <li><a href="#/messages">Messages</a></li>
-                        <li><a href="#">Friends</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <section class="is-search">
-
-                <form method="post" action="#">
-                    <input type="text" class="text" name="search" placeholder="Search" />
-                </form>
-            </section>
-
-            <section>
-                <ul class="social-links list-inline unstyled list-hover-slide">
-                    <li><a href="#">Photos</a></li>
-                    <li><a href="#">Blogs</a></li>
-                    <li><a href="#">Blank</a></li>
-                    <li><a href="#">Settings</a></li>
-                </ul>
-            </section>
-            <section class="is-calendar">
-                <div class="inner">
-                    <table>
-                        <caption>February 2013</caption>
-                        <thead>
-                            <tr>
-                                <th scope="col" title="Monday">M</th>
-                                <th scope="col" title="Tuesday">T</th>
-                                <th scope="col" title="Wednesday">W</th>
-                                <th scope="col" title="Thursday">T</th>
-                                <th scope="col" title="Friday">F</th>
-                                <th scope="col" title="Saturday">S</th>
-                                <th scope="col" title="Sunday">S</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td colspan="4" class="pad"><span>&nbsp;</span></td>
-                                <td><span>1</span></td>
-                                <td><span>2</span></td>
-                                <td><span>3</span></td>
-                            </tr>
-                            <tr>
-                                <td><span>4</span></td>
-                                <td><span>5</span></td>
-                                <td><a href="#">6</a></td>
-                                <td><span>7</span></td>
-                                <td><span>8</span></td>
-                                <td><span>9</span></td>
-                                <td><a href="#">10</a></td>
-                            </tr>
-                            <tr>
-                                <td><span>11</span></td>
-                                <td><span>12</span></td>
-                                <td><span>13</span></td>
-                                <td class="today"><a href="#">14</a></td>
-                                <td><span>15</span></td>
-                                <td><span>16</span></td>
-                                <td><span>17</span></td>
-                            </tr>
-                            <tr>
-                                <td><span>18</span></td>
-                                <td><span>19</span></td>
-                                <td><span>20</span></td>
-                                <td><span>21</span></td>
-                                <td><span>22</span></td>
-                                <td><a href="#">23</a></td>
-                                <td><span>24</span></td>
-                            </tr>
-                            <tr>
-                                <td><a href="#">25</a></td>
-                                <td><span>26</span></td>
-                                <td><span>27</span></td>
-                                <td><span>28</span></td>
-                                <td class="pad" colspan="3"><span>&nbsp;</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-
-            <!-- Copyright -->
-            <div id="copyright">
-                <p>
-                    &copy; 2014 Hexes.<br />
-                    Creators: <a href="http://djheez.us/">David Johnson & Michael Leong</a>
-                </p>
-            </div>
-
-        </div>
-
-    </div>
+    <!-- SIDE MENU -->
+    <aside>
+        <img id="hexLogo" src="images/hexlogo2.png"></img>
+        <a>
+			<svg version="1.1" id="nav-btn" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+              <path id="list-view-icon" d="M462,108.857H50V50h412V108.857z M462,167.714H50v58.857h412V167.714z M462,285.429H50v58.857h412
+             V285.429z M462,403.143H50V462h412V403.143z"/>
+            </svg>
+      	</a>
+ 
+      
+      <div id="sideMenuContainer">
+        <nav>
+          <ul>
+            <li ><a href="#/profile">Profile</a></li>
+            <p class="navSpaces"></p>
+            <li class="mobileTwoColumn"><a href="#/friends">Friends</a></li>
+            <p class="navSpaces"></p>
+            <li class="current"><a href="#/messages">Messages</a></li>
+            <p class="navSpaces"></p>
+            <li class="mobileTwoColumn"><a href="#">Photos</a></li>
+            <p class="navSpaces"></p>
+            <li><a href="#">Blogs</a></li>
+            <p class="navSpaces"></p>
+            <li class="mobileTwoColumn"><a href="#">Settings</a></li>
+          </ul>
+        </nav>
     
+    
+      </div>
+      
+      <!--<div class="vertical-line"></div>-->
+    </aside>
+    <!-- End of SIDE MENU -->
+    <article ng-app="hexApp">
+        <div id="newSwitcherWrapper">
+            <div ng-view></div>
+          <!-- NG VIEW OR WHATEVER HERE INIT -->
+        </div>
+    </article>
 
 </body>
-<script type="text/javascript">
-        window.onload = function() {
-            var sideMenuPress = true;
-            $("html").mousemove(function(){  
-                $("#content").mousemove(function(){
-                    sideMenuPress = false;
-                    //alert("hide now");	
-                    $('#skel-panels-defaultWrapper').fadeOut('fast');
-                    document.getElementById("skel-panels-defaultWrapper").style.display = "none";
-				  document.getElementById("skel-panels-defaultWrapper").style.opacity = 0;
-			});
-			if (sideMenuPress == true) {
-				$("#skel-panels-defaultWrapper").show();
-				document.getElementById("skel-panels-defaultWrapper").style.display = "block";
-				$('#skel-panels-defaultWrapper').fadeIn('slow');
-				document.getElementById("skel-panels-defaultWrapper").style.opacity = 100;
-				//alert("show now");	
-			}
-			sideMenuPress = true;	
-
-            });
-        }
-		</script>
-
-
-                                                                                                                                                                                                                                                                                                                                                    </html>
+</html>
