a11dda57b2bcaa1568d32cbbd1579857fcfb8d3c
David Johnson <dd.j@btinternet.com>
2014-03-20 15:12:25 +0000

    new accordion for view profile

--- hex/experiment/js/userProfile.js
+++ hex/experiment/js/userProfile.js
@@ -1,6 +1,26 @@
 $(document).ready(function() {
 
 var leftColumnHeight = $('#leftColumn').height();
-document.getElementById("rightColumn").style.height = leftColumnHeight-17+"px";
+document.getElementById("rightColumn").style.height = leftColumnHeight-119+"px";
+
+document.getElementById("scaleDiv").style.height = leftColumnHeight-119+"px";
+
+$( "#blogsList" ).click(function() {
+	document.getElementById("blogsListDiv").style.maxHeight = leftColumnHeight+1019+"px";
+});
+
+
+$( ".newBlogButton" ).click(function() {
+	if ( $( ".viewBlogs" ).is( ":hidden" ) ) {
+    	$( ".viewBlogs" ).slideDown( "slow" );
+		$( ".newBlogs" ).hide();
+  	} else {
+		$( ".hideBR" ).css('display','none');
+		$( ".newBlogs" ).css('visibility','visible');
+		$( ".newBlogs" ).slideDown( "slow" );
+    	$( ".viewBlogs" ).hide();
+  	}
+});
+
 
 });
