26fa8440cc3ba5cb17805d1f92d17db5a4891e4e
David Johnson <dd.j@btinternet.com>
2014-03-21 22:10:02 +0000

    added search bar for friends and filter friends and finalised register page

--- app/views/home.php
+++ app/views/home.php
@@ -45,6 +45,85 @@
         <link rel="stylesheet" href="css/personalNewsFeed.css"/>
 <link rel="stylesheet" href="css/mainStyle.css" />
 
+<style>
+.searchBar {
+  color: #666;
+  font: 85%/140% Arial, Helvetica, sans-serif;
+  width: 800px;
+	max-width: 96%;
+	margin: 0 auto;
+}
+.searchBar a {
+	color: #69C;
+	text-decoration: none;
+}
+.searchBar a:hover {
+	color: #F60;
+}
+
+/* reset webkit search input browser style */
+.searchBar input {
+	outline: none;
+}
+.searchBar input[type=search] {
+	-webkit-appearance: textfield;
+	-webkit-box-sizing: content-box;
+	font-family: inherit;
+	font-size: 100%;
+}
+.searchBar input::-webkit-search-decoration,
+.searchBar input::-webkit-search-cancel-button {
+	display: none; /* remove the search and cancel icon */
+}
+
+/* search input field */
+.searchBar input[type=search] {
+	background: #ededed url(http://i.imm.io/Q0Jo.png) no-repeat 9px center;
+	border: solid 1px #ccc;
+	padding: 9px 10px 9px 32px;
+	width: calc(100% - 42px);
+	
+	-webkit-border-radius: 10em;
+	-moz-border-radius: 10em;
+	border-radius: 10em;
+	
+	-webkit-transition: all .5s;
+	-moz-transition: all .5s;
+	transition: all .5s;
+}
+
+#searchFriends {
+	/*margin-left: calc(50% - 40px);
+	margin-left: -webkit-calc(50% - 40px);
+	margin-left: -moz-calc(50% - 40px);*/
+	margin-bottom: 20px;
+}
+
+#filterFriends {
+	width: 32%;
+}
+
+#searchFriends:focus {
+	margin-left: 1000px;	
+}
+
+.searchBar input[type=search]:focus {
+	background-color: #fff;
+	border-color: #6dcff6;
+	-webkit-box-shadow: 0 0 5px rgba(109,207,246,.5);
+	-moz-box-shadow: 0 0 5px rgba(109,207,246,.5);
+	box-shadow: 0 0 5px rgba(109,207,246,.5);
+}
+
+/* placeholder */
+.searchBar input:-moz-placeholder {
+	color: #999;
+}
+.searchBar input::-webkit-input-placeholder {
+	color: #999;
+}
+</style>
+
 </head>
 
 <body ng-app="hexApp">
@@ -66,23 +145,33 @@
 
 <li>
                 <div class="notice_box"> 
-                <span><a href="#/notifications"> Friendzoned </a></span>
+                <span><a href="#/notifications"> Notifications </a></span>
                 <span id="numOfNotifications" class="bubble_count">10</span>
                 </div>
 </li>
+            <li>  
+              <div class="searchBar">
+                <form id="searchFriends">
+                  <input type="search" placeholder="Search">
+                </form>
+              </div>
+            </li>  
             <li class="mobileTwoColumn"><a href="#/profile">Profile</a></li>
             <p class="navSpaces"></p>
-            <li class="mobileTwoColumn"><a href="#/friends">Friends</a></li>
+            <li><a href="#/friends">Friends</a></li>
             <p class="navSpaces"></p>
             <li class="mobileTwoColumn"><a href="#/messages/24">Messages</a></li>
             <p class="navSpaces"></p>
-            <li class="mobileTwoColumn"><a href="#/photos">Photos</a></li>
+            <li><a href="#/photos">Photos</a></li>
             <p class="navSpaces"></p>
-            <li><a href="#">Blogs</a></li>
+            <li class="mobileTwoColumn"><a href="#">Blogs</a></li>
             <p class="navSpaces"></p>
-            <li class="mobileTwoColumn"><a href="#/settings">Settings</a></li>
+            <li><a href="#/settings">Settings</a></li>
             <p class="navSpaces"></p>
-            <li class="mobileTwoColumn"><a href="#">Log Off</a></li>
+            <li class="mobileTwoColumn"><a href="#">Admin</a></li>
+            <p class="navSpaces"></p>
+            <li><a href="#">Log Off</a></li>
+
           </ul>
         </nav>
     
